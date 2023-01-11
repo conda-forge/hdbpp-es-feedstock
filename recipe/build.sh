3b6073c9dece2bb02c38cd3c1a7b4d22853bee31
Benjamin Bertrand <benjamin.bertrand@maxiv.lu.se>
2023-01-11 14:17:53 +0100

    Separate debug info in a new package
    hdbpp-es-dbg shall be used to install debug symbols

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,3 +5,10 @@ cmake ${CMAKE_ARGS} \
 
 cmake --build build -v -j $CPU_COUNT
 cmake --build build --target install
+
+
+# Separate debugging symbols to create separate package
+${OBJCOPY} --only-keep-debug ${PREFIX}/bin/hdb++es-srv ${PREFIX}/bin/hdb++es-srv.dbg
+chmod 664 ${PREFIX}/bin/hdb++es-srv.dbg
+${OBJCOPY} --strip-debug ${PREFIX}/bin/hdb++es-srv
+${OBJCOPY} --add-gnu-debuglink=${PREFIX}/bin/hdb++es-srv.dbg ${PREFIX}/bin/hdb++es-srv
